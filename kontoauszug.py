ba88379ea103db9c9a3061d516cf1507c6885a09
wundertaeter <47701649+wundertaeter@users.noreply.github.com>
2019-06-27 19:57:19 +0200

    Add files via upload

--- kontoauszug.py
+++ kontoauszug.py
@@ -3,14 +3,19 @@ from datetime import datetime
 import os
 
 location = os.path.dirname(os.path.abspath(__file__))
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+
+
 def find_all(name):
     result = []
     for root, dirs, files in os.walk(location):
-        if name in dirs:
-            result.append(os.path.join(root, name))
+        for dir in dirs:
+            if name.strip() in dir.strip():
+                result.append(os.path.join(root, dir))
     return result
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+
+
 class kto_ausz_Parser(object):
     def __init__(self, content):
         self.__content = content
@@ -79,6 +84,12 @@ class kto_ausz_Parser(object):
     def get_amounts(self):
         return [row[3] for row in self.rows]
 
+    def print(self):
+        for row in self.rows:
+            print(' -----------------------------------------------------------------------------------------------')
+            print('|\t{}\t|\t{}\t|\t{}\t|\t{}\t|'.format(row[0], row[1], (row[2][:10]+'...'
+                                                                          + row[2][-10:]), row[3]))
+
     def to_tsv(self, name='kontoauszug.tsv', mode='w'):
         if not 'w' in mode and not 'a' in mode:
             print('[ERROR] invalid mode "{}"'.format(mode))
@@ -94,19 +105,22 @@ class kto_ausz_Parser(object):
                     else:
                         f.write(value+'\t')
 
-#-------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------
 if __name__ == '__main__':
     for _ in range(100):
         print('')
-        
-    print('ordnernamen eingeben: ')
-    dir = input()
-    dirs = find_all(dir)
 
-    try:
-        path = dirs[0]
-    except:
-        print('Ordner "{}" nicht gefunden!'.format(dir))
+    while True:
+        print('ordnernamen eingeben: ')
+        dir = input()
+        dirs = find_all(dir)
+
+        try:
+            path = dirs[0]
+            break
+        except:
+            print('\nOrdner "{}" nicht gefunden!\n'.format(dir))
 
     if len(dirs) > 1:
         print('\n!!Vorsicht mehrere ergebnisse gefunden!!\n')
@@ -117,11 +131,11 @@ if __name__ == '__main__':
         dir = input()
         if dir != '':
             path = dir
-        
+
     print('\nnehme jetzt "{}"\n'.format(path))
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
     print('tippe "hilfe" ein fuer eine liste von kommandos:')
-    args = input()
+    args = input().strip()
     if 'hilfe' in args:
         print('\nkommandos werden durch lehrzeichen getrennt eingegeben')
         print('\n"alle" \t\t --> alle dateien im ordner werden in eine datei geschrieben')
@@ -130,11 +144,11 @@ if __name__ == '__main__':
         print(
             '"liste" \t --> es wird eine liste von allen pdf dateien im ordner "{}" ausgegeben'.format(path.split('/')[-1]))
         print('\njetzt kommando(s) eingeben? um ohne kommando(s) vortzufahren ENTER drücken\n')
-        args = input()
+        args = input().strip()
 
     list_of_files = [name for name in os.listdir(path) if name.endswith(".PDF")]
     if 'liste' in args:
-        print('LISTE aller PDF dateien im ordner "{}"'. format(path.split('/')[-1]))
+        print('\nLISTE aller PDF dateien im ordner "{}"\n'. format(path.split('/')[-1]))
         for file in list_of_files:
             print(file)
 
@@ -145,21 +159,21 @@ if __name__ == '__main__':
         mode = 'a'
     elif 'schleife' in args:
         print('\num die eingabe zu beenden bitte "fertig" eintippen')
-        name = input()
+        name = input().strip()
         while 'fertig' not in name:
             pdf_names.append(name)
-            name = input()
+            name = input().strip()
 
     else:
-        print('Bitte Dateinamen eingeben: ')
-        pdf_names.append(input())
+        print('\nBitte Dateinamen eingeben: ')
+        pdf_names.append(input().strip())
 
     if 'anhang' in args:
         mode = 'a'
     else:
         with open(path+'/kontoauszug.tsv', 'w') as f:
             pass
-#-------------------------------------------------------------------------------    
+# -------------------------------------------------------------------------------
     print('\n')
     for name in set(pdf_names):
         try:
@@ -178,5 +192,6 @@ if __name__ == '__main__':
         parsed_kto_ausz = kto_ausz_Parser(content)
         parsed_kto_ausz.to_tsv(mode=mode)
         print('{} --> {} Buchungszeilen'.format(name, parsed_kto_ausz.num_rows))
+        parsed_kto_ausz.print()
 
 print('\nfertig')
