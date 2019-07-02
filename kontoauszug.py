430a56389d2fa6995d7775c78b1b94c4a59701eb
wundertaeter <47701649+wundertaeter@users.noreply.github.com>
2019-07-02 11:38:44 +0200

    Update kontoauszug.py

--- kontoauszug.py
+++ kontoauszug.py
@@ -4,6 +4,10 @@ import PyPDF2
 from datetime import datetime
 import os
 
+#cd ~/
+#curl -o .kontoauszug.py https://github.com/wundertaeter/programs/blob/master/kontoauszug.py
+
+
 location = os.path.dirname(os.path.abspath(__file__))
 # -------------------------------------------------------------------------------
 
