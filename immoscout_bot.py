99b007ee5aa8b504dedbbf57aae82a0d35ca6bef
wundertaeter <47701649+wundertaeter@users.noreply.github.com>
2019-07-08 17:38:18 +0200

    Update immoscout_bot.py

--- immoscout_bot.py
+++ immoscout_bot.py
@@ -8,33 +8,14 @@ try:
 except:
     blacklist.cursor.execute('CREATE TABLE blacklist(title TEXT)')
 
-url = 'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Friedrichshain-Friedrichshain_Mitte-Mitte_Prenzlauer-Berg-Prenzlauer-Berg/1,50-/-/EURO--700,00/-/3,6,7,8,40,113,118,127/false/-/-/true?enteredFrom=result_list'
 
 chrome_path = r"H:\sonstiges\python\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
-driver.get(url)
 
 
-posts = driver.find_elements_by_class_name("result-list-entry__brand-title-container")
-
-
-while True:
-    posts = driver.find_elements_by_class_name("result-list-entry__brand-title-container")
-    try:
-        for post in posts:
-            title = post.text
-            if not blacklist.check('title', title):
-                break
-
-        
-        print('id: ', post.id, '\ntext: ', title)
-        post.click()
-    except Exception as e:
-        print('[ERROR]', e)
-        time.sleep(30)
-        continue
-    
-       
+def submit(link):
+    driver.get(link)
+      
     #Klick auf 'Anbieter kontaktieren'
     try:
         driver.find_element_by_xpath("""//*[@id="is24-expose-contact-bar-top"]/div/div/div[1]/div/div[2]/a""").click()
@@ -47,6 +28,9 @@ while True:
                 break
 
     
+        text_area = driver.find_element_by_id('contactForm-Message')
+        text_area.clear()
+        text_area.send_keys(u"Sehr geehrte Damen und Herren\n ....")
         last_name = driver.find_element_by_id('contactForm-lastName')
         last_name.send_keys("Oettl")
         first_name = driver.find_element_by_id('contactForm-firstName')
@@ -61,15 +45,33 @@ while True:
         post_code.send_keys("12203")
         city = driver.find_element_by_id('contactForm-city')
         city.send_keys("Berlin")
-        text_area = driver.find_element_by_id('contactForm-Message')
-        text_area.clear()
-        text_area.send_keys(u"Sehr geehrte Damen und Herren\n ....")
-
+        time.sleep(1)
         #driver.find_element_by_xpath("//button[@data-ng-click='submit()' or contains(.,'Anfrage senden')]").click()
     except Exception as e:
         print('[contactForm ERROR]', e)
 
-    blacklist.insert((title,))
-    time.sleep(5)
-    driver.execute_script("window.history.go(-1)")
     
+    
+
+if __name__ == '__main__':
+    url = 'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Friedrichshain-Friedrichshain_Mitte-Mitte_Prenzlauer-Berg-Prenzlauer-Berg/1,50-/-/EURO--700,00/-/3,6,7,8,40,113,118,127/false/-/-/true?enteredFrom=result_list'
+    while True:
+        driver.get(url)
+        posts = driver.find_elements_by_class_name("result-list-entry__brand-title-container")
+
+        flats = []
+        for post in posts:
+            flat = {}
+            flat['link'] = post.get_attribute('href')
+            flat['id'] = post.id
+            flat['title'] = post.text
+            if not blacklist.check('title', flat['link']):
+                flats.append(flat)
+                
+
+        for flat in flats:
+            print(flat['title'])
+            submit(flat['link'])
+            blacklist.insert((flat['link'],))
+        
+        time.sleep(30)
