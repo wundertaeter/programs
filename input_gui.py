9e987da8d0b650dbe674984b2b26fd1918f01dab
wundertaeter <47701649+wundertaeter@users.noreply.github.com>
2019-07-10 18:00:40 +0200

    Update input_gui.py

--- input_gui.py
+++ input_gui.py
@@ -1,12 +1,11 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
-fields = ['url','last_name', 'first_name', 
-          'email', 'street', 'house', 
-          'post_code', 'city']
 
 class gui(object):
     def __init__(self):
         self.entries = {}
+        self.fields =[]
+        self.text_field = False
 
     def fetch(self):
         entries = {}
@@ -19,27 +18,33 @@ class gui(object):
         for key, val in entries.items():    
             print('%s: "%s"' % (key, val)) 
 
-    def makeform(self, root, fields):
-        for field in fields:
-            row = Frame(root)
+    def makeform(self):
+        for field in self.fields:
+            row = Frame(self.root)
             lab = Label(row, width=15, text=field, anchor='w')
             ent = Entry(row)
             row.pack(side=TOP, fill=X, padx=5, pady=5)
             lab.pack(side=LEFT)
             ent.pack(side=RIGHT, expand=YES, fill=X)
             self.entries[field] = ent
-        text_area = ScrolledText(root, height=5, width=30)
-        text_area.pack(side=RIGHT, fill=X, expand=YES)
-        self.entries['text_field'] = text_area
+                    
+        if self.text_field:
+            text_area = ScrolledText(self.root, height=5, width=30)
+            text_area.pack(side=RIGHT, fill=X, expand=YES)
+            self.entries['text_field'] = text_area
         
     def run(self):
-        root = Tk()
-        self.makeform(root, fields)
-        search_button = Button(root, text='Start', command=self.fetch, width=15)
+        self.root = Tk()
+        self.makeform(fields)
+        search_button = Button(self.root, text='Start', command=self.fetch, width=15)
         search_button.pack(side=LEFT, fill=BOTH)
         root.mainloop()
 
 if __name__ == '__main__':
     gui = gui()
-    gui.run()
+    gui.fields = ['url','last_name', 'first_name', 
+              'email', 'street', 'house', 
+              'post_code', 'city']
+    gui.text_field = True
+    gui.run(fields)
     
