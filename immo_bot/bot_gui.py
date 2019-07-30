a764f87d0adb04afd220987bca45a60e726cba68
wundertaeter <47701649+wundertaeter@users.noreply.github.com>
2019-07-30 21:30:25 +0200

    Update bot_gui.py

--- immo_bot/bot_gui.py
+++ immo_bot/bot_gui.py
@@ -1,130 +1,237 @@
-import json
 from tkinter import *
-from tkinter.scrolledtext import ScrolledText
-#import appscript Mac os only
-import sys
-import sqlite3
-import sys
-import subprocess
+from tkinter import filedialog
+import PyPDF2
+from datetime import datetime
+import os
+import xlsxwriter
+import xlrd
+import json
+
+location = os.path.expanduser('~')
+
+# -------------------------------------------------------------------------------
 
 
-path = '/'.join(sys.argv[0].split('/')[:-1]) + '/'
-connection = sqlite3.connect('./immo.db')
-cursor = connection.cursor()
-try:
-    cursor.execute('SELECT * FROM blacklist')
-except:
-    cursor.execute('CREATE TABLE blacklist( title TEXT, link TEXT PRIMARY KEY)')
+def find_all(name):
+    result = []
+    for root, dirs, files in os.walk(location):
+        for dir in dirs:
+            if name.strip().lower() in dir.strip().lower():
+                result.append(os.path.join(root, dir))
+    return result
+# -------------------------------------------------------------------------------
 
+
+class kto_ausz_Parser(object):
+    def __init__(self, content, format_in, format_out):
+        self.__content = content
+        self.rows = []
+        self.num_rows = self.__get_num_rows()
+        for i in range(self.num_rows):
+            row = self.__parse_date(format_in, format_out)
+            row += self.__parse_description()
+            row += self.__parse_amount()
+            self.rows.append(row)
+
+    def __parse_date(self, format_in, format_out):
+        found = False
+        for i in range(len(self.__content) - 10):
+            try:
+                date1 = datetime.strptime(self.__content[i:i+10], format_in).date()
+                date2 = datetime.strptime(self.__content[i+10:i+20], format_in).date()
+                found = True
+            except:
+                pass
+
+            if found:
+                self.__content = self.__content[i+20:].strip()
+                return (datetime.strftime(date1, format_out),
+                        datetime.strftime(date2, format_out))
+
+    def __parse_description(self):
+        space = False
+        for i in range(len(self.__content)):
+            if ' ' in self.__content[i] and space:
+                description = self.__content[:i].strip()
+                self.__content = self.__content[i:].strip()
+                return (description,)
+            elif ' ' in self.__content[i]:
+                space = True
+            else:
+                space = False
+
+    def __parse_amount(self):
+        for i in range(len(self.__content)):
+            if '+' in self.__content[i] or '-' in self.__content[i]:
+                amount = self.__content[:i+1].strip()
+                self.__content = self.__content[i+1:]
+                return (amount,)
+
+    def __get_num_rows(self):
+        count = 0
+        for i in range(len(self.__content) - 10):
+            try:
+                date1 = datetime.strptime(self.__content[i:i+10], '%d.%m.%Y').date()
+                date2 = datetime.strptime(self.__content[i+10:i+20], '%d.%m.%Y').date()
+                count += 1
+            except:
+                pass
+        return count
+
+    def to_string(self):
+        s = ''
+        for row in self.rows:
+            s += ' -------------------------------------------------------------------------------------------------------------------------------------\n'
+            s += '|\t{}    \t|\t{}    \t|\t{}    \t|\t{}\t|\n'.format(row[0], row[1],
+                                                                      (row[2][:10]+'...' + row[2][-10:]), row[3])
+        s += ' -------------------------------------------------------------------------------------------------------------------------------------\n'
+        return s
+    
+    def to_xlsx(self, file):
+        wbRD = xlrd.open_workbook(file)
+        sheets = wbRD.sheets()
+
+        wb = xlsxwriter.Workbook(file)
+
+        for sheet in sheets:
+            newSheet = wb.add_worksheet(sheet.name)
+            for row in range(sheet.nrows):
+                for col in range(sheet.ncols):
+                    newSheet.write(row, col, sheet.cell(row, col).value)
+        
+        col = 0
+        row_num = sheet.nrows
+
+        for row in self.rows:
+            for value in row:
+                if value == row[-1]:
+                    if '-' in value:
+                        newSheet.write(row_num, col, value+'\t\n')
+                    elif '+' in value:
+                        newSheet.write(row_num, col+1,'\t'+value+'\n')
+                else:
+                    newSheet.write(row_num, col, value+'\t')
+                col += 1
+            row_num += 1
+            col = 0
+        
+        wb.close()
+
+
+def beautify(name, pages, num_rows, size=30):
+    for i in range(len(pages)):
+        pages[i] = '\n{}\n'.format(name) + pages[i]
+        for _ in range((size-num_rows[i]*2+2)):
+            pages[i] += '\n'
+        pages[i] += 'Buchungszeilen: {}\t\t\t\t\t\t\t\t\t\tPage {}/{}'.format(
+            num_rows[i], i+1, len(pages))
+    return pages
+
+
+def convert(pdf_names, path, mode='w'):
+    dir = location+'/Documents/excel/'
+    file = dir+'kontoauszug.xlsx'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    if not os.path.exists(file):
+        workbook = xlsxwriter.Workbook(file)
+        worksheet = workbook.add_worksheet()
+        columns = ['Datum', 'Wert', 'Erläuterung', 
+                   'Betrag Soll EUR', 'Betrag Haben EUR']
+        for i in range(len(columns)):
+            worksheet.write(0, i, columns[i])
+        workbook.close()
+
+    all_pages = []
+    for name in pdf_names:
+        pdf_file = open(path + '/' + name, 'rb')
+        read_pdf = PyPDF2.PdfFileReader(pdf_file)
+        number_of_pages = read_pdf.getNumPages()
+
+        num_rows = []
+        pages = []
+        for number in range(number_of_pages):
+            page = read_pdf.getPage(number)
+            page_content = page.extractText()
+            parsed_kto_ausz = kto_ausz_Parser(page_content, '%d.%m.%Y', '%d.%m.%Y')
+
+            if len(parsed_kto_ausz.rows) > 0:
+                s = parsed_kto_ausz.to_string()
+                pages.append(s)
+                num_rows.append(len(parsed_kto_ausz.rows))
+                parsed_kto_ausz.to_xlsx(file)
+
+        all_pages.extend(beautify(name, pages, num_rows))
+
+    return all_pages
+
+# --------------------------------------------------------------------
 
 class gui(object):
-    def __init__(self, table='', column='', drop_label='', 
-                 text_field=False, fields=[], default={}, file_to_execute='', 
-                 sql=False):
-        self.__entries = {}
-        self.default = default
-        self.fields = fields
-        self.text_field = text_field
-        self.file_to_execute = file_to_execute
-        self.sql_args = {'column': column, 'table': table}
-        self.drop_label = drop_label
-        self.sql = sql
-        self.entries = {'test': False}
-
-    def ch_mode(self):
-        self.entries['test'] = not self.entries['test']
-        if self.entries['test']:
-            self.test_button.config(bg="green")
+    def __init__(self):
+        self.root = Tk()
+        self.data = {'Directory Open': {'frame': Frame(self.root)},
+                     'Directory Save': {'frame': Frame(self.root)}}
+        if os.path.exists('entries.json'):
+            with open('entries.json', 'r', encoding='utf-8') as fp:
+                self.entries = json.load(fp)
         else:
-            self.test_button.config(bg='SystemButtonFace')
+            self.entries = {'open_dir': 'Ordner wählen', 'initialdir': '/'}
+        self.i = 0
 
-    def create_drob_down(self):
-        if self.sql:
-            cursor.execute('SELECT {column} FROM {table}'.format_map(self.sql_args))
-            list_of_tuples = cursor.fetchall()
-            choices = [tuple[0] for tuple in list_of_tuples]
-            choices.append(['refresh'])
-            self.tkvar = StringVar(self.manu_frame)
-            self.tkvar.set(self.sql_args['table'])
-            popupMenu = OptionMenu(self.manu_frame, self.tkvar, *choices)
-            popupMenu.pack(side=RIGHT, fill=BOTH, expand=YES)
-            self.manu_frame.pack(side=TOP, fill=X)
-            self.tkvar.trace('w', self.trigger_drop_down)
+    def open_file(self, *args):
+        print(self.data['Directory Open']['tkvar'].get())
 
-    def trigger_drop_down(self, *args):
-        self.sql_args['col_name'] = self.tkvar.get()
-        if 'refresh' not in self.sql_args['col_name']:
-            with connection:
-                cursor.execute("DELETE FROM {table} WHERE {column} == '{col_name}'".format_map(self.sql_args))
-        
-        for l in self.manu_frame.pack_slaves():
-            if 'optionmenu' in str(l):
-                l.destroy()
-        self.create_drob_down() 
-
-    def fetch(self):
-        for field, entry in self.__entries.items():
-            if field == 'text_area':
-                self.entries['text_area'] = entry.get("1.0",END).strip()
-            else:
-                self.entries[field] = entry.get()
-
-        with open('data.json', 'w', encoding='utf-8') as fp:
+    def dump(self, key, value):
+        self.entries[key] = value
+        with open('entries.json', 'w', encoding='utf-8') as fp:
             json.dump(self.entries, fp)
-        try:
-            subprocess.Popen([sys.executable, path + self.file_to_execute], creationflags=subprocess.CREATE_NEW_CONSOLE)
-            #appscript.app('Terminal').do_script('python3 {}/web_bot.py'.format(path)) Mac os only
-            print('starte python3 {}'.format(path + self.file_to_execute))
-        except Exception as e:
-            print(e)
 
-    def makeform(self):
-        for field in self.fields:
-            row = Frame(self.root)
-            lab = Label(row, width=15, text=field, anchor='w')
-            ent = Entry(row)
-            if field in self.default:
-                ent.insert(END, self.default[field])
-            row.pack(side=TOP, fill=X, padx=5, pady=5)
-            lab.pack(side=LEFT)
-            ent.pack(side=RIGHT, expand=YES, fill=X)
-            self.__entries[field] = ent
+    def save_file(self):
+        filename = filedialog.asksaveasfilename(initialdir = self.entries['initialdir'],title = "Select file")
+        self.dump('initialdir', '/'.join(filename.split('/')[:-1]))
 
-        if self.text_field:
-            text_area = ScrolledText(self.root, height=5, width=30)
-            if 'text_area' in self.default:
-                text_area.insert(END, self.default['text_area'])
-            text_area.pack(side=RIGHT, fill=X, expand=YES)
-            self.__entries['text_area'] = text_area
+    def open_dir(self):
+        self.dump('open_dir', filedialog.askdirectory())
+        files = [name for name in os.listdir(self.entries['open_dir']) if name.endswith('.PDF')]
+        if len(files) == 0:
+            files = ['Keine PDF Dateien gefunden']
+        self.create_drob_down('Directory Open', funk=self.open_file, label='Files', choices=files)
+
+    def create_drob_down(self, name, funk, label='', choices=['..']):
+        for ps in self.data[name]['frame'].pack_slaves():
+            if 'option' in str(ps):
+                ps.destroy()
+        self.data[name]['tkvar'] = StringVar(self.data[name]['frame'])
+        self.data[name]['tkvar'].set(label)
+
+        popupMenu = OptionMenu(self.data[name]['frame'], self.data[name]['tkvar'], *choices)
+        popupMenu.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+        self.data[name]['tkvar'].trace('w', funk)
+        self.data[name]['frame'].pack(side=TOP, fill=BOTH, expand=YES)
 
     def run(self):
-        self.root = Tk()
-        self.manu_frame = Frame(self.root)
-        self.test_button = Button(self.manu_frame, text='test', command=self.ch_mode, width=15)
-        self.test_button.pack(side=LEFT, fill=BOTH)
-        self.create_drob_down()
-
-        try:
-            with open('data.json', 'r', encoding='utf-8') as fp:
-                self.default = json.load(fp)
-            self.makeform()
-        except:
-            self.makeform()
-        search_button = Button(self.root, text='Start', command=self.fetch, width=15)
-        if self.text_field:
-            search_button.pack(side=LEFT, fill=BOTH)
+        b_text = self.entries['open_dir'].split('/')[-1]
+        Button(self.data['Directory Open']['frame'], text=b_text, command=self.open_dir).pack(side=LEFT)
+        if self.entries['open_dir'] != 'Ordner wählen':
+            files = [name for name in os.listdir(self.entries['open_dir']) if name.endswith('.PDF')]
         else:
-            search_button.pack(side=BOTTOM, fill=BOTH)
-
+            files = ['..']
+        self.create_drob_down('Directory Open', funk=self.open_file, label='File', choices=files)
+        
+        label = Label(self.root, justify=LEFT)
+        label.pack(side=TOP)
+        label.config(text='\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        
+        if self.entries['initialdir'] == '/':
+            b_text = 'Save As'
+        else:
+            b_text = self.entries['initialdir'].split('/')[-1]
+        Button(self.data['Directory Save']['frame'], text=b_text, command=self.save_file).pack(side=LEFT)
+        self.data['Directory Save']['frame'].pack(side=BOTTOM)
         self.root.mainloop()
 
-
 if __name__ == '__main__':
-    gui = gui(table='blacklist', column='title', 
-                text_field=True, file_to_execute='web_bot.py', sql=True)
-    gui.fields = ['webdriver','url','last_name', 'first_name',
-                'email', 'phone', 'street', 'house',
-                'post_code', 'city']
+    gui = gui()
     gui.run()
-
-
